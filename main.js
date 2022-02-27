7ad88d9db480173813e7cea39388db57ee755da3
tedtalksbits <tedaneblake@gmail.com>
2022-02-26 19:42:49 -0500

    :star: new window added

--- main.js
+++ main.js
@@ -24,10 +24,14 @@ function closeAppleMenu() {
 // windows
 const about_window = document.getElementById("about_window");
 const system_window = document.getElementById("system_window");
+const appstore_window = document.getElementById("appstore_window");
 
 // window togglers
 const about_window_toggle = document.getElementById("about_window_toggle");
 const system_window_toggle = document.getElementById("system_window_toggle");
+const appstore_window_toggle = document.getElementById(
+   "appstore_window_toggle"
+);
 
 // show window
 function showWindow(toggler, window) {
@@ -39,6 +43,7 @@ function showWindow(toggler, window) {
 }
 showWindow(about_window_toggle, about_window);
 showWindow(system_window_toggle, system_window);
+showWindow(appstore_window_toggle, appstore_window);
 
 // close window - window is the great grandchild of the close button
 const closeBtns = document.querySelectorAll(".close");
@@ -76,10 +81,14 @@ maxBtns.forEach((btn) => {
 // headers - element to use drag window
 const about_window_header = document.getElementById("about_window_header");
 const system_window_header = document.getElementById("system_window_header");
+const appstore_window_header = document.getElementById(
+   "appstore_window_header"
+);
 
 // make window draggable
 dragEl(about_window, about_window_header);
 dragEl(system_window, system_window_header);
+dragEl(appstore_window, appstore_window_header);
 
 // make window draggable function
 function dragEl(el, draggableHeader) {
@@ -128,6 +137,25 @@ function dragEl(el, draggableHeader) {
    }
 }
 
+/************************** Wallpapers **************************/
+
+let wallpapers = [
+   "https://images.unsplash.com/photo-1531366936337-7c912a4589a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+   "https://images.unsplash.com/photo-1642548666500-7990b88e691f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1878&q=80",
+   "https://images.unsplash.com/photo-1643635486856-819832539429?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1918&q=80",
+   "https://images.unsplash.com/photo-1531366936337-7c912a4589a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+   "https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
+   "https://images.unsplash.com/photo-1629946832022-c327f74956e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
+   "https://images.unsplash.com/photo-1634986666676-ec8fd927c23d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+   "https://images.unsplash.com/photo-1629277152917-966a1f1705cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
+   "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80",
+];
+const wallpaper_imgs_element = document.getElementById("wallpaper_imgs");
+wallpapers.map((wallpaper) => {
+   const imgElement = `<img src=${wallpaper} alt="background" class="wallpaperImg"/>`;
+   wallpaper_imgs_element.innerHTML += imgElement;
+});
+
 /************************** Change wallpaper **************************/
 const body = document.querySelector("body");
 const wallpaperImg = document.querySelectorAll(".wallpaperImg");
@@ -137,6 +165,7 @@ wallpaperImg.forEach((e) => {
       body.style.backgroundImage = `url(${e.src})`;
    });
 });
+
 /************************** datetime **************************/
 setInterval(() => {
    const date = new Date().toLocaleDateString();
@@ -225,16 +254,14 @@ const about_window_body_content = [
          <img src="images/spinner.gif" alt="loading.." style="height: 57px;" />
       </div>
       <div class="animateIn delay-1">
-         <h3>No resourses found</h3>
+         <h3>You get the picture📸...</h3>
       </div>
    </div>
    `,
 ];
-console.log(tabs);
 tabs.forEach((tab) => {
    tab.addEventListener("click", function () {
       currentIndex = tab.dataset.index;
-
       tabs.forEach((tb) => removeClass("active", tb));
       this.classList.add("active");
       about_window_body.innerHTML = about_window_body_content[currentIndex];
